1ff9daa0bdaa2ddf060c50595aa95f3016dc2b74
Maple <8961085+MapleHinata@users.noreply.github.com>
2025-12-19 21:22:33 -0300

    1.0.0.17 - Update to API14

--- Dalamud.LoadingImage/LoadingImage.cs
+++ Dalamud.LoadingImage/LoadingImage.cs
@@ -6,10 +6,12 @@ namespace Dalamud.LoadingImage
    [Sheet( "LoadingImage" )]
    public readonly struct LoadingImage(ExcelPage page, uint offset, uint row) : IExcelRow<LoadingImage>
     {
+        public uint RowOffset { get; }
         public uint RowId => row;
 
         public ReadOnlySeString Name => page.ReadString( offset, offset );
 
         public static LoadingImage Create(ExcelPage page, uint offset, uint row) => new(page, offset, row);
+        public ExcelPage ExcelPage { get; }
     }
 }
